6b1ce580c50e5a81654fad9a4550c55d5a047418
xqlcrystal <xql252660@163.com>
2016-07-30 17:12:08 +0800

    面向对象类型系统

---
+++ MsdnWebCast_OO/Employee.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Employee.aspx.cs" Inherits="MsdnWebCast_OO.Employee" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+         <asp:ScriptManager ID="ScriptManager1" runat="server">
+            <Scripts>
+                <asp:ScriptReference Path="/Employee.js" />
+            </Scripts>
+        </asp:ScriptManager>
+    <div id="info">     
+    </div>
+        <script type="text/javascript">
+            function display(text) {
+                document.getElementById("info").innerHTML+=(text+"<br/>");
+            }
+            var jeffery = new MyNameSpace.Intern("jeffrey");
+            jeffery.set_year(3);
+            display(jeffery.getDescription());
+           
+            var vendor = new MyNameSpace.Vendor("lihongxing");
+            vendor.set_year(5);
+            display(vendor.getDescription());
+
+          
+            var hepinghui = new MyNameSpace.FullTimeEmployee("hepinghui");
+            hepinghui.set_year(5);
+            display(hepinghui.getDescription());
+        </script>
+    </form>
+</body>
+</html>
---
+++ MsdnWebCast_OO/Employee.js
@@ -0,0 +1,77 @@
+﻿Type.registerNamespace("MyNameSpace");
+MyNameSpace.Employee = function (name) {
+    this._name = name ? name : "{ananymous}";
+    this._year = 0;
+}
+
+MyNameSpace.Employee.prototype = {
+    get_name: function () {
+        return this._name;
+    },
+
+    get_year: function () {
+        return this._year;
+    },
+
+    set_year: function (value) {
+        this._year = value;
+    },
+
+    calculateSalary: function () {
+        throw Error.notImplemented();
+    },
+
+    getDescription: function () {
+        return String.format("{0} get {1} yuan per month",
+            this._name,
+            this.calculateSalary());
+    }
+}
+MyNameSpace.Employee.registerClass("MyNameSpace.Employee");
+
+
+MyNameSpace.Intern = function (name) {
+    MyNameSpace.Intern.initializeBase(this, [name]);
+}
+
+MyNameSpace.Intern.prototype = {
+    calculateSalary: function () {
+        return 2000;
+    },
+
+    getDescription: function () {
+        var description = MyNameSpace.Intern.callBaseMethod(this, "getDescription");
+        return description += "what a poor inten";
+    }
+}
+
+MyNameSpace.Intern.registerClass("MyNameSpace.Intern", MyNameSpace.Employee);
+
+
+MyNameSpace.Vendor = function (name) {
+    MyNameSpace.Vendor.initializeBase(this, [name]);
+}
+
+MyNameSpace.Vendor.prototype = {
+    calculateSalary: function () {
+        return 5000 + this.get_year() * 1000;
+    }
+}
+
+MyNameSpace.Vendor.registerClass("MyNameSpace.Vendor", MyNameSpace.Employee);
+
+
+MyNameSpace.FullTimeEmployee = function (name) {
+    MyNameSpace.FullTimeEmployee.initializeBase(this, [name]);
+}
+
+MyNameSpace.FullTimeEmployee.prototype = {
+    calculateSalary: function () {
+        return 15000 + this.get_year() * 2000;
+    }
+}
+
+MyNameSpace.FullTimeEmployee.registerClass("MyNameSpace.FullTimeEmployee", MyNameSpace.Employee);
+
+
+
