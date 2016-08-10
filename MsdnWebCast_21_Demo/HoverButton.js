d5c9bd92948b267cc1ad2e94d0c6cab771123962
xqlcrystal <xql252660@163.com>
2016-08-10 19:49:17 +0800

    hoverbutton

---
+++ MsdnWebCast_21_Demo/HoverButton.js
@@ -0,0 +1,116 @@
+﻿///<reference name="MicrosoftAjax.js"/>
+
+Type.registerNamespace("Demo");
+
+Demo.HoverButton = function (element) {
+    Demo.HoverButton.initializeBase(this, [element]);
+
+    this._clickDelegate = null;
+    this._hoverDelegate = null;
+    this._unhoverDelegate = null;
+}
+
+Demo.HoverButton.prototype = {
+    get_text: function () {
+        return this.get_element().innerHTML;
+    },
+
+    set_text: function (value) {
+        this.get_element().innerHTML = value;
+    },
+
+    add_click: function (handler) {
+        this.get_events().addHandler("click", handler);
+    },
+
+    remove_click: function(handler) {
+        this.get_events().removeHandler("click", handler);
+    },
+
+    add_hover: function(handler) {
+        this.get_events().addHandler("hover", handler);
+    },
+
+    remove_hover: function (handler) {
+        this.get_events().removeHandler("hover", handler);
+    },
+
+    add_unhover: function(handler) {
+        this.get_events().addHandler("unhover", handler);
+
+    },
+
+    remove_unhover: function(handler) {
+        this.get_events().removeHandler("unhover", handler);
+    },
+
+    initialize: function () {
+        var element=this.get_element();
+        if (!element.tabindex) element.tabindex = 0;
+
+        if (this._clickDelegate == null) {
+            this._clickDelegate = Function.createDelegate(this, this._clickHandler);
+            Sys.UI.DomEvent.addHandler(element, "click", this._clickDelegate);
+        }
+
+        if (this._hoverDelegate == null) {
+            this._hoverDelegate = Function.createDelegate(this, this._hoverHandler);
+            Sys.UI.DomEvent.addHandler(element, "mouseover", this._hoverDelegate);
+            Sys.UI.DomEvent.addHandler(element, "foucs", this._hoverDelegate);
+        }
+
+        if (this._unhoverDelegate == null) {
+            this._unhoverDelegate = Function.createDelegate(this, this._unhoverHandler);
+            Sys.UI.DomEvent.addHandler(element, "mouseout", this._unhoverDelegate);
+            Sys.UI.DomEvent.addHandler(element, "blur", this._unhoverDelegate);
+        }
+
+        Demo.HoverButton.callBaseMethod(this, "initialize");
+    },
+
+    _clickHandler: function(event) {
+        var handler = this.get_events().getHandler("click");
+        if (handler) {
+            handler(this, Sys.EventArgs.Empty);
+        }
+    },
+
+    _hoverHandler: function (event) {
+        var handler = this.get_events().getHandler("hover");
+        if (handler) {
+            handler(this, Sys.EventArgs.Empty);
+        }
+    },
+
+    _unhoverHandler: function (event) {
+        var handler = this.get_events().getHandler("unhover");
+        if (handler) {
+            handler(this, Sys.EventArgs.Empty);
+        }
+    },
+
+    dispose: function () {
+        var element = this.get_element();
+        if (this._clickDelegate) {
+            Sys.UI.DomEvent.removeHandler(element, "click", this._clickDelegate);
+            delete this._clickDelegate;
+        }
+
+        if (this._hoverDelegate) {
+            Sys.UI.DomEvent.removeHandler(element, "foucs", this._hoverDelegate);
+            Sys.UI.DomEvent.removeHandler(element, "mouseover", this._hoverDelegate);
+            delete this._hoverDelegate;
+        }
+
+        if (this._unhoverDelegate) {
+            Sys.UI.DomEvent.removeHandler(element, "blur", this._unhoverDelegate);
+            Sys.UI.DomEvent.removeHandler(element, "mouseout", this._unhoverDelegate);
+            delete this._unhoverDelegate;
+        }
+
+        Demo.HoverButton.callBaseMethod(this, "dispose");
+    }
+}
+
+
+Demo.HoverButton.registerClass("Demo.HoverButton", Sys.UI.Control);
---
+++ MsdnWebCast_21_Demo/HoverButtonTest.aspx
@@ -0,0 +1,71 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="HoverButtonTest.aspx.cs" Inherits="MsdnWebCast_21_Demo.HoverButtonTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:ScriptManager ID="ScriptManager1" runat="server">
+            <Scripts>
+                <asp:ScriptReference Path="~/HoverButton.js" />
+            </Scripts>
+        </asp:ScriptManager>
+
+        <script type="text/javascript">
+            var app = Sys.Application;
+            app.add_init(applicationInithandler);
+
+            function applicationInithandler(sender, args) {
+               
+                
+                $create(
+                    Demo.HoverButton,
+                   {
+                       text: "A hover button control",
+                       element: {
+                           style: {
+                               "font-weight": "bolder",
+                               "border-width":"2px"
+                           }
+                       }
+                   },
+                    {
+                        "click": start,
+                        "hover": doSomethingOnHover,
+                        "unhover": doSomethingOnUnhover
+
+                    },
+                    null,
+                    $get("button1")
+                    );
+
+               
+            }
+
+            function start(sender,args) {
+                alert("The start function handler ");
+            }
+
+
+            function doSomethingOnHover(sender,args) {
+                var hovermessage="this is hover message";
+                $get("HoverLabel").innerHTML = hovermessage;
+            }
+
+            function doSomethingOnUnhover(sender,args) {
+                $get("HoverLabel").innerHTML = "";
+            }
+        </script>
+        <button type="button" id="button1" />
+        
+    </div>
+       <div id="HoverLabel"></div> 
+    </form>
+</body>
+</html>
